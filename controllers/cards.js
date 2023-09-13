d8adc0510a0a184b350b0eed27448a1589640c12
Nikita Petukhov <ortiespn@yandex.ru>
2023-09-13 12:13:55 +0300

    do all func

--- controllers/cards.js
+++ controllers/cards.js
@@ -12,7 +12,12 @@ const sendStatus500 = function (res) {
   res.status(500).send({ message: 'На сервере произошла ошибка' });
 };
 
-module.exports.getCards = (req, res) => {};
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .populate(['owner', 'likes'])
+    .then((cards) => res.send(cards))
+    .catch(() => sendStatus500(res));
+};
 
 module.exports.addCard = (req, res) => {
   const { name, link } = req.body;
@@ -30,8 +35,55 @@ module.exports.addCard = (req, res) => {
     });
 };
 
-module.exports.deleteCard = (req, res) => {};
+module.exports.deleteCard = (req, res) => {
+  if (req.params.cardId.length === 24) {
+    Card.findByIdAndRemove(req.params.cardId)
+      .then((card) => {
+        if (!card) {
+          sendStatus404(res);
+          return;
+        }
+        res.send({ message: 'Данные удалены' });
+      })
+      .catch(() => sendStatus500(res));
+  } else {
+    const err = { message: 'Неверный формат Id' };
+    sendStatus400(res, err);
+  }
+};
 
-module.exports.likeCard = (req, res) => {};
+module.exports.likeCard = (req, res) => {
+  if (req.params.cardId.length === 24) {
+    Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+      .populate(['owner', 'likes'])
+      .then((card) => {
+        if (!card) {
+          sendStatus404(res);
+          return;
+        }
+        res.send(card);
+      })
+      .catch(() => sendStatus500(res));
+  } else {
+    const err = { message: 'Неверный формат Id' };
+    sendStatus400(res, err);
+  }
+};
 
-module.exports.unlikeCard = (req, res) => {};
+module.exports.unlikeCard = (req, res) => {
+  if (req.params.cardId.length === 24) {
+    Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+      .populate(['owner', 'likes'])
+      .then((card) => {
+        if (!card) {
+          sendStatus404(res);
+          return;
+        }
+        res.send(card);
+      })
+      .catch(() => sendStatus500(res));
+  } else {
+    const err = { message: 'Неверный формат Id' };
+    sendStatus400(res, err);
+  }
+};
